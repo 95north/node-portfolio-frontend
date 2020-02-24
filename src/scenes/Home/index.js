4347e3d4f446f8b57f72c39722b5dff664008e66
Tori Sigler <mssigler@gmail.com>
2020-02-24 15:05:44 -0500

    Set up Feature Based File Structure

---
+++ src/scenes/Home/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import CSSModules from 'react-css-modules';
+
+import styles from './styles.scss';
+
+
+
+class Home extends Component {
+
+    render(){
+
+        return(
+            <p>HOME Scene</p>
+        )
+    }
+
+} 
+
+export default Home; 
